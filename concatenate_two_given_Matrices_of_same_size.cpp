51c84ddc7d9927cce4daac320f1291bd704810ec
Soumyadeep Pal <64325089+soumyadeep2002ss@users.noreply.github.com>
2020-10-03 17:32:33 +0530

    Update concatenate_two_given_Matrices_of_same_size.cpp

--- concatenate_two_given_Matrices_of_same_size.cpp
+++ concatenate_two_given_Matrices_of_same_size.cpp
@@ -1,65 +1,62 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
-int i,j,m,n,A[10][10],B[10][10],store[10][10];
- cout << "Enter number of rows and columns for first matrix & second matrix: \n";
+    int i, j, m, n, A[10][10], B[10][10], store[10][10];
+    cout << "Enter number of rows and columns for first matrix & second matrix: \n";
     cin >> m >> n;
     cout << "Enter the elements of First matrix: \n";
-    for(i=0;i<m;i++)
+    for (i = 0; i < m; i++)
     {
-        for(j=0;j<n;j++)
+        for (j = 0; j < n; j++)
         {
             cin >> A[i][j];
         }
     }
     cout << "\nEnter the elements of Second matrix: \n";
-    for(i=0;i<m;i++)
+    for (i = 0; i < m; i++)
     {
-        for(j=0;j<n;j++)
+        for (j = 0; j < n; j++)
         {
             cin >> B[i][j];
         }
     }
     cout << "\nEntered Matrix(First): " << endl;
-      for(i=0;i<m;i++)
+    for (i = 0; i < m; i++)
     {
-        for(j=0;j<n;j++)
+        for (j = 0; j < n; j++)
         {
-            cout << A[i][j]<< " " ;
+            cout << A[i][j] << " ";
         }
-        cout<<"\n";
+        cout << "\n";
     }
     cout << "\nEntered Matrix(Second): " << endl;
-      for(i=0;i<m;i++)
+    for (i = 0; i < m; i++)
     {
-        for(j=0;j<n;j++)
+        for (j = 0; j < n; j++)
         {
-            cout << B[i][j]<< " " ;
+            cout << B[i][j] << " ";
         }
-        cout<<"\n";
+        cout << "\n";
     }
-      for(i=0;i<m;i++)
+    for (i = 0; i < m; i++)
     {
-        for(j=0;j<n;j++)
+        for (j = 0; j < n; j++)
         {
-           store[i][j]=A[i][j] ;
-           store[i][j+n]=B[i][j] ;
+            store[i][j] = A[i][j];
+            store[i][j + n] = B[i][j];
         }
     }
-     cout << "\nConcatenated Matrix: " << endl;
-   
-      for(i=0;i<m;i++)
+    cout << "\nConcatenated Matrix: " << endl;
+
+    for (i = 0; i < m; i++)
     {
-        for(j=0;j<2*n;j++)
+        for (j = 0; j < 2 * n; j++)
         {
-          cout<< store[i][j]<< " " ;
+            cout << store[i][j] << " ";
         }
-        cout<<"\n";
+        cout << "\n";
     }
-
-
-
-return 0;
+    return 0;
 }
